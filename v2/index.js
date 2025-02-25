21de4314052e3c291c7632fdd5d24de4ff4e1279
Eduardo Verone <du_verone_19@hotmail.com>
2025-02-25 00:51:45 -0300

    Comit inicial

---
+++ v2/index.js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+const fs = require('fs');
+
+
+// instanciar o browser ↓
+const launchBrowser = async () => {
+	return await puppeteer.launch({
+		headless: "new",
+		args: ["--window-size=1920,1080"],
+	});
+};
+
+
+// apos estanciado, criar uma página dentro dele ↓ ▼
+
+const convertHTML = async () => {
+	const browser = await launchBrowser();
+
+	const page = await browser.newPage();
+
+  const html = await fs.promises.readFile('index.js', 'utf8')
+  
+
+  // setar conteudo ↓
+	await page.setContent("<p>Bem vindo - VERONE TECH</p>");
+
+	// testando
+  const nome = "Nome"
+  // conversão do conteúdo para PDF ↓
+	await page.pdf({
+		format: "A4",
+		landscape: false,
+		width: 1920,
+		height: 1080,
+		path: "veronetech.pdf",
+	});
+
+
+  // no final, descarregamos a pagina e o browser (fecha o browser para nao consumir memoria do PC)
+  await page.close();
+  await browser.close();
+
+};
+
+convertHTML();
